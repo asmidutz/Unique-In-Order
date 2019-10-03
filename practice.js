48f4ca7dc5dffedbae951dbc20b4ab45c011d908
asmidutz <asmidutz@netflix.com>
2019-10-03 14:04:00 -0700

    Troubleshooting, about to pull out a nested if statement.

--- practice.js
+++ practice.js
@@ -1,45 +1,65 @@
 
 function sorty(newarr){
-    console.log(newarr + " sorty func")
+console.log("++++  Start Sorty Function  ++++")
+console.log("newarr is " + newarr)
     var i;
     var n = 1;
+
     for (i = 0; i < newarr.length; i++) {
-    
+        console.log("Enter for Loop")
+        if (newarr.length === 1)
+        {
+            
+            console.log ("newarr = 1 exit function")
+            console.log("newarr = " + newarr)
+            return newarr; 
+      
+        }
         if (n > newarr.length -1){
-            console.log(newarr + " return")
+
+            console.log("if n is more than the newarr length exit function")
             return newarr;
         }
         if (newarr[i] === newarr[n]) {
-           console.log("We have a match")
+           console.log("Enter Duplicate found statement")
            var character1 = newarr[n]
            var character2 = newarr[i]
            var char1
            var char2
             if (character1 === character1.toUpperCase()) {
                 char1 = true
+                console.log("Enter check if lowercase")
             }
-            else {char1 = false}
+            else {char1 = false
+            console.log("first character false")}
             if (character2 === character2.toUpperCase()){
                 char2 = true
+                console.log("Enter second check if lowercase")
             }
-            else {char2 = false}
+            else {char2 = false
+                console.log("first character false")}
 
             if (char1 === char2){
-            console.log(char1 + "   "  + char2)
-            console.log( "Char1 is equal to char2")
+            console.log("Duplicate found - splicing " + newarr[n])
             newarr.splice(n,1)
-       
+         
+            
+            console.log("newarr is now " + newarr)
             end = newarr.length -1
        
             almend = newarr.length -2
-        
-            if (newarr[end] === newarr[almend]) {
-                newarr.splice(end,1)
-        
+            console.log("newarr[end] = " + newarr[end] + " newarr[almend] = " + newarr[almend])
+                if (newarr[end] === newarr[almend]) {
+                    console.log("splicing duplicate found at end of newarr array, newarr before splice = " + newarr )
+                    newarr.splice(end,1)
+                    console.log("newarr after splice = " + newarr)
                 
-            }
-        
-             sorty(newarr)
+                    
+                    
+                }
+                
+                console.log("==== Calling Sorty function again ====")
+                sorty(newarr)
 
             }
 
@@ -58,21 +78,21 @@ function uniqueInOrder(iterable){
    {
     
      var test = sorty(iterable)
-     console.log("return first test")
+ 
      return test
    }
     else
     {
       var newarr = iterable.split("");
       var test = sorty(newarr)
-      console.log("return second test " + test)
+   
       
      return test
     }  
   }
 
-  var str1 = "AAA"
-
+  var str1 = "AAAB"
+  ///var str1 = ['A','A','A']
   var result = uniqueInOrder(str1)
   
 console.log(result)
---
+++ simplefunc.js
@@ -0,0 +1,8 @@
+function test(){
+arr = ['a']
+return arr
+
+}
+
+result = test()
+console.log(result)
